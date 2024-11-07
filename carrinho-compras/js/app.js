b1ccea3798384edc5a7c36ceb5a7849d86c6ecf4
taitwelve <taynascimento15@gmail.com>
2024-11-07 18:04:17 -0300

    fix: função de adicionarao carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,12 +1,12 @@
 function adicionar() {
-    //recuperar valores nome do produto, quantidade e valor 
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1]; 
-    let quantidade = document.getElementById('quantidade'); 
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
-}     
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+    </section>`;
+}    
+
