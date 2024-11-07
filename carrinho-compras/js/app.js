6dccc0de45c99265089c7a2f205b9c3dad40ff76
taitwelve <taynascimento15@gmail.com>
2024-11-07 17:17:43 -0300

    fix: ajuste na função de recuperar valores

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,5 +1,12 @@
 function adicionar() {
     //recuperar valores nome do produto, quantidade e valor 
-    let nomeProduto = document.getElementById('produto'); 
-    let quantidade = document.getElementById('quantidade');
-}    
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1]; 
+    let quantidade = document.getElementById('quantidade'); 
+    alert(nomeProduto);
+    alert(valorUnitario);
+    alert(quantidade.value);
+    let preco = quantidade.value * valorUnitario;
+    alert(preco);
+}     
